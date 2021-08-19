e1e9b3fe96949f351d9a22913b06d24b61b3b931
YiChiChao <katelyn.c0818@gmail.com>
2021-08-19 18:06:00 +0800

    bubble sort

---
+++ Bubble Sort/bubble_sort1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void bubble_sort(int* array, int n){
+    int t = 0;
+    int swap = 0;
+    while(!(swap == 0 && t != 0)){
+        swap = 0;
+        for(int i = 0; i < n-t-1; ++i){
+            if(array[i] > array[i+1]){
+                int tmp = array[i];
+                array[i] = array[i+1];
+                array[i+1] = tmp;
+                swap += 1;
+            }
+        }
+        t += 1;
+
+    }
+}
+int main(){
+    int array[6] = {5, 1, 4, 2, 8};
+    bubble_sort(array, 5);
+    for(int i = 0; i < 5; ++i){
+        printf("%d%c", array[i], ','*(i != 4) + '\n'*(i == 4));
+    }
+    return 0;
+}
